7fb2ce2d20ca642500f5374cfc4ddcbe4b00e932
mark1036 <realmarcusyip@gmail.com>
2016-10-05 09:57:41 +0800

    quiz
    first game

---
+++ quiz/js/index.js
@@ -0,0 +1,43 @@
+function quiz(){
+  
+  var question1 = prompt("how many fingers does the average person have on one hand?");
+  if (question1 === "5") {
+    alert("correct");
+  }
+  else{
+    alert("try again");
+  }
+  
+  var question2 = prompt("how many hands does the average person have");
+  if (question2 === "2") {
+    alert("great you can count");
+  }
+  
+  else{
+    alert("no do u even count bro")
+    
+  }
+    
+var question3 = prompt("how do trump save the planet");
+  if (question3 === "build a wall"){
+    alert("so u support trump")
+    
+  }
+  else{
+    alert("either way it wont be effective")
+    
+  }
+  
+  var question4 = prompt("how do i get to america?");
+  if (question4 === "take a plane"){
+    alert("yea")
+  }
+else{ alert("then how you tell me ")
+    }
+  
+  var question5 = prompt("do u want to build a snowman");
+  if(question4 === "no"){
+    alert ("thank you")
+  }
+  else{ alert("please watch something other than frozen")}
+}//do not delete this bracket, it's the closing bracket for the function
